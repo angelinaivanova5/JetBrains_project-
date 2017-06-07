7ab3ea45cac4027dbcddcd2a0fd46820d6d2f80c
serejap <timur312@mail.ru>
2017-06-07 13:39:11 +0300

    закрытый элемент

--- app/src/main/java/com/example/user/myapplication/Main2Activity.java
+++ app/src/main/java/com/example/user/myapplication/Main2Activity.java
@@ -2,7 +2,6 @@ package com.example.user.myapplication;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
 import android.content.Intent;
@@ -12,6 +11,7 @@ public class Main2Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+        String txt = getIntent().setStringExtra("code")
         final Button button = (Button) findViewById(R.id.bin4);
         final Main2Activity this_ = this;
         View.OnClickListener listener = new View.OnClickListener() {
--- app/src/main/java/com/example/user/myapplication/Main3Activity.java
+++ app/src/main/java/com/example/user/myapplication/Main3Activity.java
@@ -3,13 +3,11 @@ package com.example.user.myapplication;
 import android.os.Bundle;
 
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
 import android.content.Intent;
 
 public class Main3Activity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,9 +17,20 @@ public class Main3Activity extends AppCompatActivity {
         View.OnClickListener listener = new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                String btnText = button.getText().toString();
+                Button button1 = (Button) findViewById(R.id.bin20);
+                String btnText1 = button1.getText().toString();
+                if (btnText1.equals("")) {
+                    button1 = (Button) findViewById(R.id.bin20);
+                } else {
+                    button1 = (Button) findViewById(R.id.bin21);
+                }
+                button1.setText(btnText);
                 Intent intent = new Intent(this_, Main2Activity.class);
+                intent.putExtra("name",name.getText().toString());
                 startActivity(intent);
             }
         };
+        button.setOnClickListener(listener);
     }
 }
