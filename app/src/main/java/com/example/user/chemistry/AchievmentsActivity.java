f55224e5ec7a7757dfefbaef7c7497071aee12e5
nastyabadikova <nastyabadikova@yandex.ru>
2017-06-08 13:55:04 +0300

    дизайн достижений и базовых элементов

--- app/src/main/java/com/example/user/chemistry/AchievmentsActivity.java
+++ app/src/main/java/com/example/user/chemistry/AchievmentsActivity.java
@@ -1,25 +1,35 @@
 package com.example.user.chemistry;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 
 public class AchievmentsActivity extends AppCompatActivity {
-    public void onCreate(Bundle savedInstanceState)
-
-    {
+    final public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.achievments);
         Intent intent = getIntent();
         int k = intent.getIntExtra("AAAAAAA", -1);
-        if (k > 1) {
+        if (k >= 1) {
             ImageButton button1 = (ImageButton) findViewById(R.id.imageButton1);
             button1.setImageResource(android.R.drawable.checkbox_on_background);
-            Button button2 = (Button) findViewById(R.id.button1);
+            TextView button2 = (TextView) findViewById(R.id.button1);
             button2.setText("Юный Химик");
+            button2.setBackgroundColor(0xFFAFF36B);
+        }
+        if (k >= 2) {
+            ImageButton button3 = (ImageButton) findViewById(R.id.imageButton2);
+            button3.setImageResource(android.R.drawable.checkbox_on_background);
+            TextView button4 = (TextView) findViewById(R.id.button2);
+            button4.setText("Лучший в мире");
+            button4.setBackgroundColor(0xFFAFF36B);
         }
     }
 }
